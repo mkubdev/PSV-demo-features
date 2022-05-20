997e5ce06cb3e66ae46055173fa7cb4d32f53116
Maxime TFT <mkubik@tftlabs.com>
2022-05-20 16:12:48 +0200

    Update tailwind deps + switch config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+
+/* Add 'false' to disable the useEffect running twice */
 const nextConfig = {
   reactStrictMode: false,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
